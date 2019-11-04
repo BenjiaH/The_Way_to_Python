ea101d7bb889ddeb64ac0252d9985ef797ac21d0
BenjiaH <huyaowen.benjia@stu.cdu.edu.cn>
2019-11-04 20:00:28 +0800

    OOP 使用metaclass

--- CH V OOP/23.Metaclass.py
+++ CH V OOP/23.Metaclass.py
@@ -31,4 +31,26 @@ class Book(metaclass=ItemMetaClass):
 class CellPhone(metaclass=ItemMetaClass):
     __slots__ = ("price", "_discount")
 
-    def __init__(self):
+    def __init__(self, price):
+        self.price = price
+
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, discount):
+        self._discount = discount
+
+
+b = Book("疯狂Python讲义", 89)
+b.discount = 0.76
+print(b.cal_price())
+cp = CellPhone(2399)
+cp.discount = 0.85
+print(cp.cal_price())
+
+"""
+从上面的代码可见，通过使用metaclass可以动态修改程序中的一批类，对他们集中进行某种修改。这个功能在开发一些基础性框架时非常有用，程序
+可以通过使用metaclass为一批需要具有通用功能的类添加方法
+"""
