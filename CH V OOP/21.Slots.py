e271a882d7ac442b215cc37eaa141ffe89e9c4b3
BenjiaH <huyaowen.benjia@stu.cdu.edu.cn>
2019-11-04 15:16:43 +0800

    OOP __slots__函数限制绑定属性

--- CH V OOP/21.Slots.py
+++ CH V OOP/21.Slots.py
@@ -16,3 +16,17 @@ d.walk = MethodType(lambda self: print("{}正在慢慢走".format(self.name)), d
 d.age = 5
 d.walk()
 # d.foo() = 30
+
+Dog.bar = lambda self: print("abc")
+d.bar()
+
+
+class GunDog(Dog):
+    def __init__(self, name):
+        super(GunDog, self).__init__(name)
+        pass
+
+
+gd = GunDog("Puppy")
+gd.speed = 90
+print(gd.speed)
