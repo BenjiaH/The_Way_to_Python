d09fd073a6e9cef903a6f801d3014f0f9490c288
BenjiaH <huyaowen.benjia@stu.cdu.edu.cn>
2019-11-04 19:21:30 +0800

    OOP 使用metaclass

--- CH V OOP/23.Metaclass.py
+++ CH V OOP/23.Metaclass.py
@@ -8,3 +8,27 @@ class ItemMetaClass(type):
         # 为该类动态添加一个cal_price方法
         attrs["cal_price"] = lambda self: self.price * self.discount
         return type.__new__(cls, name, bases, attrs)
+
+
+# 定义book类
+class Book(metaclass=ItemMetaClass):
+    __slots__ = ("name", "price", "_discount")
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, discount):
+        self._discount = discount
+
+
+# 定义cellphone类
+class CellPhone(metaclass=ItemMetaClass):
+    __slots__ = ("price", "_discount")
+
+    def __init__(self):
