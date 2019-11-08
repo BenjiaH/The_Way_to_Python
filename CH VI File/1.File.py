0be2acf26696ee913b1f1768e1e169c5d8a304e8
BenjiaH <huyaowen.benjia@stu.cdu.edu.cn>
2019-11-08 17:21:14 +0800

    文件读写

--- CH VI File/1.File.py
+++ CH VI File/1.File.py
@@ -1,5 +1,5 @@
 # -*-coding:utf-8-*-
 
-f = open("E:\\Users\\Administrator\Desktop\\23test.txt", encoding="utf-8")
+f = open("E:\\Users\\Administrator\Desktop\\23test.txt", encoding="utf-8", buffering=True)
 print(f.read())
 f.close()
