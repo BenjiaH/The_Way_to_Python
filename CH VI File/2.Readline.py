7ac99a294e165fd1adb011cd2defb00227376fd5
BenjiaH <huyaowen.benjia@stu.cdu.edu.cn>
2019-11-08 20:55:46 +0800

    文件按行读取内容

--- CH VI File/2.Readline.py
+++ CH VI File/2.Readline.py
@@ -1,6 +1,6 @@
 # -*-coding:utf-8-*-
 
-f = open("E:\\Users\\Administrator\Desktop\\23test.txt", "r", True, "utf-8", )
+f = open("E:\\Users\\Administrator\Desktop\\23test.txt", "r", True, "utf-8")
 while True:
     line = f.readline()
     if not line:
