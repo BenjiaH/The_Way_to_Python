1b3035196a0afc8762e2269a11efd94cf1a79e9a
BenjiaH <huyaowen.benjia@stu.cdu.edu.cn>
2019-11-08 20:56:07 +0800

    文件使用for迭代读取内容

--- CH VI File/3.Forfile.py
+++ CH VI File/3.Forfile.py
@@ -1,6 +1,6 @@
 # -*-coding:utf-8-*-
 
-f = open("E:\\Users\\Administrator\Desktop\\23test.txt", "r", True, "utf-8", )
+f = open("E:\\Users\\Administrator\Desktop\\23test.txt", "r", True, "utf-8")
 for line in f:
     print(line, end="")
 f.close()
