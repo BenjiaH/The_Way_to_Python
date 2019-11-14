897e1a8a744fbf7d5b9e0c5eea7aa9eb4a16ec85
BenjiaH <huyaowen.benjia@stu.cdu.edu.cn>
2019-11-14 21:53:12 +0800

    用继承来创建线程

--- CH 9 Thread/2.ThreadInherit.py
+++ CH 9 Thread/2.ThreadInherit.py
@@ -14,5 +14,19 @@ class MyThread(threading.Thread):
         while self.i < 100:
             # 调用threading模块的curren_thread()函数获取当前线程
             # 调用线程对象的getName()方法获取当前线程的名字
-            print(threading.Thread.getName() + " " + str(self.i))
+            print(threading.current_thread().getName() + " " + str(self.i))
             self.i += 1
+
+
+# 主程序（主线程的线程执行体）
+for i in range(100):
+    # 调用threading模块的current_thread()函数获取当前进程
+    print(threading.current_thread().getName() + " " + str(i))
+    if i == 20:
+        # 创建并启动第一个线程
+        mt1 = MyThread()
+        mt1.start()
+        # 创建并启动第二个线程
+        mt2 = MyThread()
+        mt2.start()
+print("主线程执行完毕！")
