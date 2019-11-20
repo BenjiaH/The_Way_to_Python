c7b9f522786b8d41b14e47363c6a9fb83c314416
BenjiaH <huyaowen.benjia@stu.cdu.edu.cn>
2019-11-20 21:59:59 +0800

    线程锁_取钱

--- CH 9 Thread/6.Lock/draw.py
+++ CH 9 Thread/6.Lock/draw.py
@@ -9,7 +9,9 @@ def draw(Account, draw_amount):
 
 
 # 创建一个账户
-acct = Account.Account("1234567", 1000)
+acct1 = Account.Account("1", 1000)
+acct2 = Account.Account("2", 1000)
 # 使用两个线程模拟从同一个账户中取钱
-threading.Thread(name="甲", target=draw, args=(acct, 800)).start()
-threading.Thread(name="乙", target=draw, args=(acct, 800)).start()
+threading.Thread(name="A", target=draw, args=(acct1, 800)).start()
+threading.Thread(name="B", target=draw, args=(acct1, 800)).start()
+threading.Thread(name="C", target=draw, args=(acct1, 0)).start()
