964aeb2b56fc80815a609da3b17ac79af3705dd3
BenjiaH <huyaowen.benjia@stu.cdu.edu.cn>
2019-11-20 15:38:51 +0800

    线程锁_account文件

---
+++ CH 9 Thread/6.Lock/Account.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import threading
+import time
+
+
+class Account:
+    def __init__(self, account_no, balance):
+        self.account_no = account_no
+        self.balance = balance
+        self.lock = threading.RLock()
+
+    def get_balance(self):
+        return self.balance
+
+    def draw(self, draw_amount):
+        # 加锁
+        self.lock.acquire()
+        try:
+            if self.balance >= draw_amount:
+                # 吐出钞票
+                print(threading.current_thread().name + "取钱成功！吐出钞票：" + str(draw_amount))
+                time.sleep(0.001)
+                # 修改余额
+                self._balance -= draw_amount
+                print("\t余额为：" + str(self._balance))
+            else:
+                print(threading.current_thread().name + "取钱失败！余额不足！")
+        finally:
+            # 修改完成，释放锁
+            self.lock.release()
