670a1f8c41a1e0f014b7c4efbbc37edc14834476
benjia <benjia.h@qq.com>
2020-02-22 11:35:01 +0800

    Update README.md

--- CH 6 File/2.Readline.py
+++ CH 6 File/2.Readline.py
@@ -1,6 +1,7 @@
 # -*-coding:utf-8-*-
 
-f = open("E:\\Users\\Administrator\Desktop\\23test.txt", "r", True, "utf-8")
+# f = open("D:\\OneDrive\\Python_project\\CSDN_Python\\CH 6 File\\abc.txt", "r", True, "utf-8")
+f = open(r"D:\OneDrive\Python_project\CSDN_Python\CH 6 File\abc.txt", "r", True, "utf-8")
 while True:
     line = f.readline()
     if not line:
---
+++ README.md
@@ -0,0 +1,2 @@
+![](https://raw.githubusercontent.com/BenjiaH/cloudimg/master/data/img20200222113324.png)
+# The Way to Python.
